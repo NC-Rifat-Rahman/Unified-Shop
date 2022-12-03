728e8c9c6bb184b7593fdea89db5fb3fe3e49b1d
NC-Rifat-Rahman <rahmanrifat61@gmail.com>
2022-12-03 21:11:24 +0600

    Product-Details-&_Add-to-cart

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -30,8 +30,8 @@ const Home = () => {
           <MetaData title="ECOMMERCE" />
 
           <div className="banner">
-            <p>Welcome to Ecommerce</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <p>Welcome to Unified Shop</p>
+            <h1>Grab your Products!</h1>
 
             <a href="#container">
               <button>
